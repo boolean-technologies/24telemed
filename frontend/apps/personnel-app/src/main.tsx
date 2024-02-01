01b2e9ff95330d49c84d54b45af03a7f8b31f2f2
Ademola101 <ademolaogunmokun@yahoo.com>
2024-02-01 06:19:45 +0000

    add toast container

--- frontend/apps/personnel-app/src/main.tsx
+++ frontend/apps/personnel-app/src/main.tsx
@@ -5,7 +5,8 @@ import  {VideoSDKRTCComponent}  from '@local/videosdk-rtc-component';
 import App from './app/app';
 import '../src/index.css'
 import { PersonnelCommunicationProvider } from '@local/websocket';
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -13,6 +14,24 @@ root.render(
   <StrictMode>
     <BrowserRouter>
       <PersonnelCommunicationProvider>
+      <ToastContainer
+      toastClassName={() =>
+        "relative flex py-4 px-3 rounded overflow-hidden cursor-pointer bg-white shadow-lg"
+      }
+      bodyClassName={() => "text-black text-base font-normal"}
+      position="bottom-left"
+      autoClose={4000}
+      hideProgressBar={true}
+      newestOnTop={false}
+      closeButton={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+        
         <VideoSDKRTCComponent/>
           
         
--- frontend/packages/videosdk-rtc-component/README.md
+++ frontend/packages/videosdk-rtc-component/README.md
@@ -1,7 +1,18 @@
-# videosdk-rtc-component
 
-This library was generated with [Nx](https://nx.dev).
+# Video SDK Component Integration Guide
 
-## Running unit tests
+This repository contains the Video SDK component that we will integrate into our application. The component consists of several sub-components, styled using Tailwind CSS. You can import the package into any application using the following syntax:
 
-Run `nx test videosdk-rtc-component` to execute the unit tests via [Vitest](https://vitest.dev/).
+```javascript
+import { VideoSDKRTCComponent } from '@local/videosdk-rtc-component';
+```
+
+To use and customize this package, please refer to the documentation provided by the Video SDK team. The documentation and APIs can be found [here](https://docs.videosdk.live/api-reference/realtime-communication/intro).
+
+## Usage Example
+
+For example, to integrate the component into the personnel app, you can run the following command:
+
+```
+npx nx serve personnel-app
+```
