29e48c7a48f9e20b3aab90956363451841815663
Ademola Ogunmokun <ademolaogunmokun@yahoo.com>
2024-02-13 20:25:28 +0000

    create the input component

--- frontend/packages/shared-components/src/Input/Input.stories.tsx
+++ frontend/packages/shared-components/src/Input/Input.stories.tsx
@@ -1,8 +1,35 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj, StoryFn } from '@storybook/react';
 import { Input, InputProps } from './Input';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../config';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { FormProvider } from 'react-hook-form';
+import { FaBeer } from 'react-icons/fa';
+
 type Story = StoryObj<typeof Input>;
+
+type FormDataType = {
+  input: string;
+};
+
+const Template: StoryFn<InputProps> = (args) => {
+  const schema = yup.object().shape({
+    input: yup.string().required(),
+  });
+
+  const method = useForm<FormDataType>({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      input: '',
+    },
+  });
+
+  console.log(method.watch('input'));
+
+  return <Input {...args} control={method.control} value= {method.watch('input')} />;
+};
 
 const meta: Meta<typeof Input> = {
   component: Input,
@@ -16,28 +43,115 @@ const meta: Meta<typeof Input> = {
   ],
 };
 
-
-
-export const Default: Story = {
-  args: {
-    value: 'Hello',
-    onChange: (value: string) => console.log(value),
-  },
-};
-
-export const WithIcon: Story = {
-  args: {
-    value: 'Hello',
-    onChange: (value: string) => console.log(value),
-    icon: 'https://via.placeholder.com/20',
-  },
-};
-
-export const Empty: Story = {
-  args: {
-    value: '',
-    onChange: (value: string) => console.log(value),
-  },
+export const Default = Template.bind({});
+Default.args = {
+  name: 'input',
+  label: 'Field Label',
+  placeholder: 'Type here...',
+  type: 'text',
 };
 
 export default meta;
+
+export const InputTextWithoutLabel = Template.bind({});
+
+InputTextWithoutLabel.args = {
+  name: 'input',
+  placeholder: 'Type here...',
+  type: 'text',
+  value: '',
+};
+
+export const InputTextwithRightIcon = Template.bind({});
+
+InputTextwithRightIcon.args = {
+  name: 'input',
+  label: 'Field Label',
+  placeholder: 'Type here...',
+  type: 'text',
+  value: '',
+  inputRightIcon: <FaBeer />,
+};
+
+export const InputTextwithLeftIcon = Template.bind({});
+InputTextwithLeftIcon.args = {
+  name: 'input',
+  label: 'Field Label',
+  placeholder: 'Type here...',
+  type: 'text',
+  value: '',
+  inputLeftIcon: <FaBeer />,
+};
+
+export const InputTextWithHelperText = Template.bind({});
+InputTextWithHelperText.args = {
+  name: 'input',
+  label: 'Field Label',
+  placeholder: 'Type here...',
+  type: 'text',
+  value: '',
+  helperText: 'This is a helper text',
+};
+
+export const InputTextWithError = Template.bind({});
+
+InputTextWithError.args = {
+  name: 'input',
+  label: 'Field Label',
+  placeholder: 'Type here...',
+  type: 'text',
+  value: '',
+  error: 'This is an error',
+};
+
+export const InputFileWithLabel = Template.bind({});
+
+InputFileWithLabel.args = {
+  name: 'input',
+  label: 'Upload file',
+  type: 'file',
+  value: '',
+  placeholder: 'Add attachment...',
+};
+
+export const InputFileWithHelperText = Template.bind({});
+
+InputFileWithHelperText.args = {
+  name: 'input',
+  label: 'Upload file',
+  type: 'file',
+  value: '',
+  placeholder: 'Add attachment...',
+  helperText: 'This is a helper text',
+};
+
+export const InputFileWithError = Template.bind({});
+
+InputFileWithError.args = {
+  name: 'input',
+  label: 'Upload file',
+  type: 'file',
+  value: '',
+  placeholder: 'Add attachment...',
+  error: 'This is an error',
+};
+
+export const InputFileWithValue = Template.bind({});
+
+InputFileWithValue.args = {
+  name: 'input',
+  label: 'Upload file',
+  type: 'file',
+  value: 'file.txt',
+  placeholder: 'Add attachment...',
+};
+
+export const PasswordInput = Template.bind({});
+
+PasswordInput.args = {
+  name: 'input',
+  label: 'Password',
+  type: 'password',
+  value: '',
+  placeholder: 'Type here...',
+};
--- frontend/packages/shared-components/src/config.ts
+++ frontend/packages/shared-components/src/config.ts
@@ -1,6 +1,20 @@
 export const theme = {
   colors: {
-    primary: '#9747FF',
-    secondary: '#00C081',
+    primary: '#c4c4c4',
+    secondary: '#9747FF',
+    tertiary: '#00C081',
+    error: '#ED5555',
+    helper:"#9747FF",
+    black: '#1D1D1D'
   },
-};
+  fonts: {
+    primary: '"Inter", sans-serif',
+  },
+  
+
+  fontSizes: {
+    sm: '14px',
+    md: '16px',
+    lg: '20px',
+  },
+} as const;
