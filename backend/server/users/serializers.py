75d086f8e2a706152f92217356bc1c68129a5c2a
Omoyele Samuel <samuelomoyele777@gmail.com>
2024-02-14 21:57:17 +0000

    Patient record management system

--- backend/server/users/serializers.py
+++ backend/server/users/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, PatientRecord, MedicalRecord
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = '__all__'
+
+class PatientRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PatientRecord
+        fields = '__all__'
+
+class MedicalRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MedicalRecord
         fields = '__all__'
--- backend/server/users/views.py
+++ backend/server/users/views.py
@@ -1,7 +1,39 @@
 from rest_framework import viewsets
 from .models import User
-from .serializers import UserSerializer
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import PatientRecord, MedicalRecord
+from .serializers import UserSerializer, PatientRecordSerializer, MedicalRecordSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class PatientRecordViewSet(viewsets.ModelViewSet):
+    queryset = PatientRecord.objects.all()
+    serializer_class = PatientRecordSerializer
+    permission_classes = [IsAuthenticated]
+
+    @action(detail=False, methods=['get'])
+    def search(self, request):
+        queryset = self.queryset
+        first_name = request.query_params.get('first_name', None)
+        last_name = request.query_params.get('last_name', None)
+        phone_number = request.query_params.get('phone_number', None)
+
+        if first_name:
+            queryset = queryset.filter(first_name__icontains=first_name)
+        if last_name:
+            queryset = queryset.filter(last_name__icontains=last_name)
+        if phone_number:
+            queryset = queryset.filter(phone_number__icontains=phone_number)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class MedicalRecordViewSet(viewsets.ModelViewSet):
+    queryset = MedicalRecord.objects.all()
+    serializer_class = MedicalRecordSerializer
+    permission_classes = [IsAuthenticated]
