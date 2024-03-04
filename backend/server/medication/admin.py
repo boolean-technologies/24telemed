2098d8aacdbc6caad812545bd59f30fb90fc75bf
Omoyele Samuel <samuelomoyele777@gmail.com>
2024-03-04 14:30:21 +0000

    made call log model, medical encounters and prescribed drugs models read only respoecively in ddjango admin

--- backend/server/medication/admin.py
+++ backend/server/medication/admin.py
@@ -5,9 +5,25 @@ class DrugAdmin(admin.ModelAdmin):
     list_display = ('name', 'manufacturer', 'strength')
 
 class MedicalEncounterAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+        
+    def has_change_permission(self, request):
+        return False
+    
+    def has_delete_permission(self, request):
+        return False
     list_display = ('doctor', 'patient', 'reason_for_visit')
 
 class PrescribedDrugAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+        
+    def has_change_permission(self, request):
+        return False
+    
+    def has_delete_permission(self, request):
+        return False
     list_display = ('drug', 'dosage', 'frequency')
 
 admin.site.register(Drug, DrugAdmin)
