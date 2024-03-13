7cd0f4fa1d34b2745a620847d654b20d583a7642
oluwatobimaxwell <tobisholanke@gmail.com>
2024-03-13 21:33:35 +0200

    updated

--- frontend/apps/personnel-app/src/pages/PatientRegistionPage/FormHeader/StepState.tsx
+++ frontend/apps/personnel-app/src/pages/PatientRegistionPage/FormHeader/StepState.tsx
@@ -41,7 +41,7 @@ export function StepState({ state, position, name, onStep }: StepStateProps) {
         style={{ opacity: state === 'pending' ? 0.5 : 1, transition: "opacity 0.75s ease-out" }}
       >
         <Typography align="center" color="primary1.lighter">
-          Step {position}
+          Step {position + 1}
         </Typography>
         <Typography variant="bodyLg" weight="bold" align="center">
           {name}
@@ -79,8 +79,9 @@ const StyledStepCircle = styled(Flex)<{
   ${({ state, theme }) =>
     state === 'progress'
       ? css`
-          outline: 2px solid ${theme.palette.primary2.main};
+          outline: 3px solid ${theme.palette.primary2.main};
           outline-offset: 2px;
+          outline-background: red;
         `
       : ''}
 `;
