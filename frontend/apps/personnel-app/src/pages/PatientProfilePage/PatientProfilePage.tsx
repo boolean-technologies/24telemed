3c574a7a0506258e81a7f9d7fa335515c838a22e
oluwatobimaxwell <tobisholanke@gmail.com>
2024-03-14 09:56:19 +0200

    handled patient profile closing

--- frontend/apps/personnel-app/src/pages/PatientProfilePage/PatientProfilePage.tsx
+++ frontend/apps/personnel-app/src/pages/PatientProfilePage/PatientProfilePage.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useGetPatient } from '../../api/patient';
 import {
   Flex,
@@ -8,21 +8,36 @@ import {
 import styled from 'styled-components';
 import PatientProfileHeader from './PatientProfileHeader';
 import Tab from './Tab';
+import { Drawer } from 'antd';
+import { Modal } from 'antd-mobile';
 export function PatientProfilePage() {
   const { patientId } = useParams();
   const { data: patient, isLoading } = useGetPatient(patientId || '');
+  const navigate = useNavigate();
+
+  const onCloseProfile = () => {
+    Modal.confirm({
+      title: "Close Profile",
+      content: "Are you sure you want to close this patient profile?",
+      confirmText: "Yes, Close",
+      cancelText: "Cancel",
+      onConfirm: () => navigate(-1),
+    })
+  }
 
   if (isLoading) return <PageLoading />;
 
   if (!patient) return null;
   
   return (
-    <Root direction="column" fullHeight fullWidth>
-      <PatientProfileHeader patient={patient} />
-      <TabContainer align="center" justify="center">
-        <Tab patient={patient} />
-      </TabContainer>
-    </Root>
+    <Drawer title="Patient Profile" onClose={onCloseProfile} open width="100%">
+      <Root direction="column" fullHeight fullWidth gap="lg">
+        <PatientProfileHeader patient={patient} />
+        <TabContainer align="center" justify="center">
+          <Tab patient={patient} />
+        </TabContainer>
+      </Root>
+    </Drawer>
   );
 }
 
