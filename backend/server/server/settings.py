f5d02ab2e34624e088c5b1dd9e07d6a208665794
oluwatobimaxwell <tobisholanke@gmail.com>
2024-03-19 10:05:49 +0200

    updated settings

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -91,7 +91,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('localhost', 6379)],
+            "hosts": [os.getenv('REDIS_TLS_URL', 'redis://localhost:6379')],
         },
     },
 }
