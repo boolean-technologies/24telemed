3de80bd2625efccb2143770ff3eeee72ad899021
oluwatobimaxwell <tobisholanke@gmail.com>
2024-03-21 18:17:22 +0200

    added joinning screen

--- frontend/packages/videosdk-rtc-component/src/newUi/VideoCall.tsx
+++ frontend/packages/videosdk-rtc-component/src/newUi/VideoCall.tsx
@@ -2,6 +2,8 @@ import { MeetingProvider } from '@videosdk.live/react-sdk';
 import { AppMain } from './components/AppMain';
 import { AppContextType, CallContextProvider } from './context/AppContext';
 import { usePermissions } from './hooks/usePermissions';
+import { useState } from 'react';
+import { JoiningArea } from './components/JoiningArea';
 
 type VideoCallProps = {
   participantName: string;
@@ -19,12 +21,25 @@ export function VideoCall({
   patientId,
 }: VideoCallProps) {
   usePermissions();
+  const [screen, setScreen] = useState<'joining' | 'call'>('joining');
+  const [micEnabled, setMicEnabled] = useState<boolean>(false);
+  const [webcamEnabled, setWebcamEnabled] = useState<boolean>(false);
+  if (screen === 'joining')
+    return (
+      <JoiningArea
+        setWebcamEnabled={setWebcamEnabled}
+        webcamEnabled={webcamEnabled}
+        micEnabled={micEnabled}
+        setMicEnabled={setMicEnabled}
+        onJoinNow={() => setScreen("call")}
+      />
+    );
   return (
     <MeetingProvider
       config={{
         // TODO: Add a joining screen so user can enable or disable their mic / cam before joining
-        micEnabled: false,
-        webcamEnabled: false,
+        micEnabled,
+        webcamEnabled,
         meetingId: meetingId,
         name: participantName,
         participantId: userId,
@@ -32,7 +47,11 @@ export function VideoCall({
       token={import.meta.env.VITE_VIDEO_SDK_TOKEN}
       joinWithoutUserInteraction
     >
-      <CallContextProvider userId={userId} patientId={patientId} userType={userType}>
+      <CallContextProvider
+        userId={userId}
+        patientId={patientId}
+        userType={userType}
+      >
         <AppMain meetingTitle="Medical consultation" />
       </CallContextProvider>
     </MeetingProvider>
---
+++ frontend/packages/videosdk-rtc-component/src/newUi/components/JoiningArea/JoiningArea.tsx
@@ -0,0 +1,191 @@
+import { Flex, IonIcon, Typography } from '@local/shared-components';
+import { Avatar, Button } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { useEffect, useRef } from 'react';
+import styled from 'styled-components';
+import { IconButton } from '../IconButton';
+import { Link } from 'react-router-dom';
+
+type JoiningAreaProps = {
+  webcamEnabled: boolean;
+  setWebcamEnabled: React.Dispatch<React.SetStateAction<boolean>>;
+  micEnabled: boolean;
+  setMicEnabled: React.Dispatch<React.SetStateAction<boolean>>;
+  onJoinNow: () => void;
+};
+
+export function JoiningArea({
+  webcamEnabled,
+  setWebcamEnabled,
+  micEnabled,
+  setMicEnabled,
+  onJoinNow,
+}: JoiningAreaProps) {
+  const displayName = 'Tobi';
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    const getMediaStream = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true, // Request video stream
+          audio: false, // Request audio stream
+        });
+
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      } catch (error) {
+        console.error('Error accessing media devices:', error);
+      }
+    };
+
+    if (webcamEnabled) {
+        getMediaStream();
+      } else {
+        if (videoRef.current && videoRef.current.srcObject) {
+          const tracks = (videoRef.current.srcObject as MediaStream).getTracks();
+          tracks.forEach(track => track.stop());
+          videoRef.current.srcObject = null;
+        }
+      }
+
+    return () => {
+      if (videoRef.current && videoRef.current.srcObject) {
+        const tracks = (videoRef.current.srcObject as MediaStream).getTracks();
+        tracks.forEach((track) => track.stop());
+      }
+    };
+  }, [webcamEnabled]);
+
+  return (
+    <StyledRoot>
+      <Flex gap="xl" xsGap="md" xsDirection="column">
+        <Flex justify="center">
+          <StyledVideoWrapper>
+            <StyledVideo ref={videoRef} autoPlay playsInline controls={false} />
+            <MediaControl justify="center" padding="md">
+              <IconButton
+                icon={webcamEnabled ? 'videocam' : 'videocam-off'}
+                tooltip={webcamEnabled ? 'Turn off video' : 'Turn on video'}
+                variant={webcamEnabled ? 'primary2' : undefined}
+                onClick={() => setWebcamEnabled((prev) => !prev)}
+              />
+              <IconButton
+                icon={micEnabled ? 'mic' : 'mic-off'}
+                onClick={() => setMicEnabled((prev) => !prev)}
+                tooltip={micEnabled ? 'Turn off mic' : 'Turn on mic'}
+                variant={micEnabled ? 'primary2' : undefined}
+              />
+            </MediaControl>
+            {webcamEnabled ? null : (
+              <VideoOffOverlay justify="center">
+                <Avatar
+                  size={65}
+                  icon={displayName ? undefined : <UserOutlined />}
+                >
+                  {displayName ? (
+                    <Typography
+                      weight="bold"
+                      color="common.white"
+                      variant="bodyXl"
+                    >
+                      {
+                        <Typography
+                          weight="bold"
+                          color="common.white"
+                          variant="bodyXl"
+                        >
+                          {displayName.charAt(0)}
+                        </Typography>
+                      }
+                    </Typography>
+                  ) : null}
+                </Avatar>
+              </VideoOffOverlay>
+            )}
+          </StyledVideoWrapper>
+        </Flex>
+        <Flex direction="column" justify="center">
+          <Typography
+            variant="bodyLg"
+            color="common.white"
+            align="center"
+            weight="bold"
+          >
+            Ready to join?
+          </Typography>
+          <Flex>
+            <Link to="/">
+            <Button
+              type="primary"
+              danger
+              shape="round"
+              size="middle"
+              style={{ fontWeight: 'bold' }}
+            >
+              Cancel
+            </Button>
+            </Link>
+            <Button
+              shape="round"
+              icon={<IonIcon name="navigate" />}
+              size="middle"
+              style={{ fontWeight: 'bold' }}
+              onClick={onJoinNow}
+            >
+              Join now
+            </Button>
+          </Flex>
+        </Flex>
+      </Flex>
+    </StyledRoot>
+  );
+}
+
+const StyledRoot = styled(Flex)`
+  height: 100vh;
+  width: 100wv;
+  background: ${({ theme }) => theme.palette.primary1.main};
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledVideo = styled.video`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 0;
+`;
+
+const StyledVideoWrapper = styled.div`
+  width: 600px;
+  height: 375px;
+  max-width: 90% !important;
+  border-radius: ${({ theme }) => theme.spacing.sm};
+  overflow: hidden;
+  position: relative;
+  background-color: ${({ theme }) => theme.palette.primary1.lighter};
+`;
+
+const MediaControl = styled(Flex)`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 4;
+`;
+
+const VideoOffOverlay = styled(Flex)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 2;
+  background-color: ${({ theme }) => theme.palette.primary1.lighter};
+`;
