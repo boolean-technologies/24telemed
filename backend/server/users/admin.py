34228312afe12377f06366ee01d811e2aa1e2b74
Omoyele Samuel <samuelomoyele777@gmail.com>
2024-03-26 14:38:06 +0100

    added users to admin panel

--- backend/server/users/admin.py
+++ backend/server/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('get_user_name','user_type', 'phone_number')
+
+    def get_user_name(self, obj):
+        return f'{obj.first_name} {obj.last_name}'
+    get_user_name.short_description = "Name"
+
+admin.site.register(User, UserAdmin)
