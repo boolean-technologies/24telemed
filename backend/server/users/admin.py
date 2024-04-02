a74419d44be5d20c8d9e59895b89ed8d9baad26e
Omoyele Samuel <samuelomoyele777@gmail.com>
2024-04-02 11:11:33 +0100

    refactored user app admin

--- backend/server/users/admin.py
+++ backend/server/users/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import User
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('get_user_name','user_type', 'phone_number')
+    list_display = ('username', 'get_user_name','user_type', 'phone_number')
 
     def get_user_name(self, obj):
         return f'{obj.first_name} {obj.last_name}'
-    get_user_name.short_description = "Name"
+    get_user_name.short_description = "Full Name"
 
 admin.site.register(User, UserAdmin)
