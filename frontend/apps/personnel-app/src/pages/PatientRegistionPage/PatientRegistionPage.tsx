ec00049bfd5b407bfe2ec78165a4c0e411fda6c7
ademolaogunmokun <ademolaogunmokun@yahoo.com>
2024-04-16 09:28:30 +0000

    use the steps

--- frontend/apps/personnel-app/src/pages/PatientRegistionPage/PatientRegistionPage.tsx
+++ frontend/apps/personnel-app/src/pages/PatientRegistionPage/PatientRegistionPage.tsx
@@ -1,78 +1,88 @@
 import { useNavigate } from 'react-router-dom';
 import { PageHeader } from '../../components/PageLayout';
 import { Card, Flex } from '@local/shared-components';
-import { FormHeader } from './FormHeader';
 import styled from 'styled-components';
 import { StepStateProps } from './FormHeader/StepState';
-import { Form, Layout } from 'antd';
+import { Form, Layout, Steps } from 'antd';
 import { useFormWizardNavigation } from './useFormWizardNavigation';
 import { BioDataPage } from './FormPages/BioDataPage';
 import { MedicalDataPage } from './FormPages/MedicalDataPage';
 import { ContactDataPage } from './FormPages/ContactDataPage';
 import { PreviewPage } from './FormPages/PreviewPage';
 import { Button } from 'antd-mobile';
+import { useState, useEffect } from 'react';
 
+const steps = [
+  {
+    title: 'Bio Data',
+    content: <BioDataPage />,
+  },
+  {
+    title: 'Medical Data',
+    content: <MedicalDataPage />,
+  },
+  {
+    title: 'Contact Data',
+    content: <ContactDataPage />,
+  },
+  {
+    title: 'Preview',
+    content: <PreviewPage />,
+  }
+]
 export function PatientRegistionPage() {
+  
+
   const navigate = useNavigate();
-
-  const { step, goToStep, goBack, goNext } = useFormWizardNavigation(4);
-
-  const steps: StepStateProps[] = [
-    {
-      state: 'completed',
-      position: 0,
-      name: 'Bio Data',
-      onStep: () => goToStep(0),
-    },
-    {
-      state: 'progress',
-      position: 1,
-      name: 'Medical Data',
-      onStep: () => goToStep(1),
-    },
-    {
-      state: 'pending',
-      position: 2,
-      name: 'Contact Data',
-      onStep: () => goToStep(2),
-    },
-    {
-      state: 'pending',
-      position: 3,
-      name: 'Preview',
-      onStep: () => goToStep(3),
-    },
-  ];
-
   const onFinish = (data: any) => {
     console.log(data);
   };
+  const [current, setCurrent] = useState<number>(0)
+  const next = () => {
+    setCurrent(current + 1)
+  }
+  const prev = () => {
+    setCurrent(current - 1)
+  }
+
+  // scroll to top
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [ current ]);
 
   return (
     <Layout>
       <PageHeader title="New Patient" />
       <StyledRoot padding="md" direction="column">
-        <FormHeader steps={steps} current={step} total={steps.length} />
         <Card fullHeight>
           <Form name="newPatient" layout="vertical" onFinish={onFinish}>
-            {step === 0 ? <BioDataPage /> : null}
-            {step === 1 ? <MedicalDataPage /> : null}
-            {step === 2 ? <ContactDataPage /> : null}
-            {step === 3 ? <PreviewPage /> : null}
+            <Steps current={current}
+
+            >
+              {steps.map((item) => (
+                <Steps.Step key={item.title} title={item.title} />
+              ))}
+            </Steps>
+            {steps[current].content}
+            {current < steps.length - 1 && (
+              <Button onClick={next} type="primary">
+                Next
+              </Button>
+            )}
+            {current === steps.length - 1 && (
+              <Button onClick={prev} type="primary">
+                Previous
+              </Button>
+            )}
+            {current === steps.length - 1 && (
+              <Button onClick={() => navigate('/patient-list')} type="primary">
+                Submit
+              </Button>
+            )}
           </Form>
         </Card>
-        <Flex fullWidth justify="space-between">
-          {step === 0 ? <div /> : <Button onClick={goBack}>Previous</Button>}
-          {step === steps.length - 1 ? (
-            <Button color="primary" onClick={goNext}>
-              Submit
-            </Button>
-          ) : (
-            <Button color="primary" onClick={goNext}>
-              Next
-            </Button>
-          )}
-        </Flex>
+
       </StyledRoot>
     </Layout>
   );
