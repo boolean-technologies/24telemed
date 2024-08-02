d8f240842fdf6126eeea6e3f1dcbb659713976e8
oluwatobimaxwell <tobisholanke@gmail.com>
2024-08-02 15:02:43 +0300

    updated personnel authentication

--- backend/server/utils/permission.py
+++ backend/server/utils/permission.py
@@ -4,7 +4,7 @@ class PersonnelPermission(BasePermission):
     def has_permission(self, request, view):
         if not request.user.is_authenticated:
             return False
-        return request.user.user_type == 'personnel' or request.user.user_type == 'customer'
+        return request.user.user_type in ['personnel', 'customer']
 
 class DoctorPermission(BasePermission):
     def has_permission(self, request, view):
