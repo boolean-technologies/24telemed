65f830b9d3918a1c88723670d83768e427b61bc0
Omoyele Samuel <samuelomoyele777@gmail.com>
2024-08-06 19:48:22 +0100

    modified patient filter class

--- backend/server/patient/filter.py
+++ backend/server/patient/filter.py
@@ -3,6 +3,7 @@ from .models import Patient
 
 class PatientFilter(filters.FilterSet):
     phone_number = filters.CharFilter(lookup_expr='exact')
+    patient_id = filters.CharFilter(lookup_expr='exact')
     class Meta:
         model = Patient
         fields = ['phone_number', 'patient_id']
