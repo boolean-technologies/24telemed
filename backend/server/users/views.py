33d5f95bf8db62ea774b232eca187a864f5fa2df
Tobi Sholanke <tobisholanke@gmail.com>
2024-08-17 05:15:59 +0000

    removed unused imports

--- backend/server/users/views.py
+++ backend/server/users/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-from random import randint
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
